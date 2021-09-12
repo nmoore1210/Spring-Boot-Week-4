74d6b37462f0fd0bb0dc8b83d5e640576c0ff364
Nathan Moore <nmoore1210@msn.com>
2021-09-11 17:28:33 -0700

    first commit

---
+++ src/main/java/com/promineotech/jeep/entity/Jeep.java
@@ -0,0 +1,28 @@
+package com.promineotech.jeep.entity;
+
+import java.math.BigDecimal;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Getter
+@Setter
+public class Jeep {
+
+	private Long modelPK;
+	private JeepModel modelId;
+	private String trimLevel;
+	private int numDoors;
+	private int wheelSize;
+	private BigDecimal basePrice;
+
+}
